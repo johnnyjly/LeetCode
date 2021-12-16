04c9e7520f14d91e73d469abaa0206bf2612d36e
Lingyun Jiang <rac.angel.jly@gmail.com>
2021-12-16 11:38:55 -0500

    TwoSum exercise

---
+++ main/src/TwoSum.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> tempMap = new HashMap<>();
+        int[] output = new int[2];
+        int counter = 0;
+        for(int num: nums){
+            if(!tempMap.containsKey(num)){
+                tempMap.put((target - num), counter);
+            }
+            else{
+                output[0] = tempMap.get(num);
+                output[1] = counter;
+                return output;
+            }
+            counter ++;
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        int[] output = twoSum(nums, target);
+        assert output != null;
+        assert output[0] == 2;
+        assert output[1] == 7;
+    }
+}
+
